9e5745d97870f63f994cf586d82577a124cf4ff1
non-disclosed-CHP <popsicletomato@gmail.com>
2024-12-12 01:14:53 +0800

    Update App.js

--- src/pages/src/App.js
+++ src/pages/src/App.js
@@ -1,1 +1,20 @@
-##
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import './styles/styles.css'; // Import custom CSS
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
